75fd817ffd20a8ab98c226e2aa21b61cf550d645
eyu <none>
2024-11-07 19:50:21 +0800

    简化参数解析，移除冗余选项并调整结果保存路径

--- common/arguments.py
+++ common/arguments.py
@@ -9,29 +9,14 @@ from datetime import datetime
 def get_parser():
     parser = ArgumentParser()
 
-    parser.add_argument("--note", type=str, default="debug")
-
-    allowed_apis = ["together", "huggingface", "llama", "vllm", "debug", "gpt3.5-turbo"]
-    parser.add_argument(
-        "--api",
-        type=str,
-        choices=allowed_apis,
-        default="vllm",
-        help=f"API to use: Choose from {allowed_apis}.",
-    )
+    parser.add_argument("--api", type=str, default="vllm")
 
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--verbose", action="store_true")
 
-    #! WandB settings
-    parser.add_argument(
-        "--wandb_mode", type=str, default="disabled", choices=["disabled", "online"]
-    )
-
     #! LLM settings
     parser.add_argument("--model_ckpt", required=True)
 
-    parser.add_argument("--model_parallel", action="store_true")
     parser.add_argument("--half_precision", action="store_true")
 
     parser.add_argument("--max_tokens", type=int, default=1024, help="max_tokens")
@@ -39,30 +24,15 @@ def get_parser():
     parser.add_argument("--top_k", type=int, default=40, help="top_k")
     parser.add_argument("--top_p", type=float, default=0.95, help="top_p")
     parser.add_argument("--num_beams", type=int, default=1, help="num_beams")
-    # parser.add_argument('--repetition_penalty', type=float, default=1.1, help='repetition_penalty')
-    parser.add_argument(
-        "--max_num_worker",
-        type=int,
-        default=3,
-        help="maximum number of workers for dataloader",
-    )
+
     parser.add_argument("--test_batch_size", type=int, default=1)  # batch_size
     parser.add_argument(
         "--tensor_parallel_size", type=int, default=1
     )  # tensor_parallel_size
 
-    #! prompt settings
-    parser.add_argument("--prompts_root", default="prompts")
-
     #! dataset settings
     parser.add_argument("--data_root", default="data")
-    allowed_dataset_names = ["MATH", "GSM8K", "GSM8KHARD", "STG", "SVAMP", "MULTIARITH"]
-    parser.add_argument(
-        "--dataset_name",
-        required=True,
-        choices=allowed_dataset_names,
-        help=f"Test dataset name: Choose from {allowed_dataset_names}.",
-    )
+    parser.add_argument("--dataset_name", required=True)
 
     # do generate 和 do eval 结果的保存路径
     parser.add_argument("--gene_result", type=str, default="gene_result")
@@ -74,35 +44,18 @@ def get_parser():
 def post_process_args(args):
     # Set up logging
     suffix = "---[" + args.note + "]" if args.note is not None else ""
-    model_name = args.model_ckpt.split("/")[-1]
-    args.run_outputs_dir = os.path.join(
-        args.run_outputs_root,
+    model_name = args.model_ckpt.split("/")[-1]  # 模型名称
+    args.gene_result = os.path.join(
+        args.gene_result,
         args.dataset_name,
         model_name,
-        f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}" + suffix,
     )
-    os.makedirs(args.run_outputs_dir, exist_ok=True)
-
-    args.answer_sheets_dir = os.path.join(args.run_outputs_dir, "answer_sheets")
-    os.makedirs(args.answer_sheets_dir, exist_ok=True)
-
-    # Check GPU
-    num_gpus = torch.cuda.device_count()
-    cuda_devices = [torch.cuda.get_device_name(i) for i in range(num_gpus)]
-    assert len(cuda_devices) > 0, "No GPU available."
-    args.cuda_0 = cuda_devices[0]
-    args.cuda_1 = cuda_devices[1] if len(cuda_devices) > 1 else None
-    args.cuda_2 = cuda_devices[2] if len(cuda_devices) > 2 else None
-    args.cuda_3 = cuda_devices[3] if len(cuda_devices) > 3 else None
-
-    if len(cuda_devices) == 1:
-        if args.cuda_0 == "NVIDIA A100-SXM4-40GB" and not args.half_precision:
-            print("Warning! A100-SXM4-40GB is used, but half_precision is not enabled.")
-
+    os.makedirs(args.gene_result, exist_ok=True)
     return args
 
 
+# 保存调用脚本时候的参数, 是好的要学习
 def save_args(args):
     # Save args as json
-    with open(os.path.join(args.run_outputs_dir, "args.json"), "w") as f:
+    with open(os.path.join(args.gene_result, "args.json"), "w") as f:
         json.dump(vars(args), f, indent=4)
