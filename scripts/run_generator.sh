2cfb922fa86badad55fb2ca1e51651b3cfd359f2
eyu <none>
2024-11-13 12:04:26 +0800

    添加详细输出

--- scripts/run_generator.sh
+++ scripts/run_generator.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python run_src/do_generate.py \
     --model_ckpt mistralai/Mistral-7B-v0.1 \
     --num_rollouts 16 \
     --tensor_parallel_size 4 \
-    --verbose True \
+    --verbose
