8fc00a3a51b1322a975df511e61447b3c69fd125
eyu <none>
2024-11-22 09:50:26 +0800

    update

--- scripts/run_discriminator.sh
+++ scripts/run_discriminator.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=3 python run_src/do_discriminate.py \
+CUDA_VISIBLE_DEVICES=0,2 python run_src/do_discriminate.py \
     --gene_result_dir gene_result/mbpp_Mistral-7B-v0.1/Mistral-7B-v0.1_1 \
-    --model_ckpt microsoft/Phi-3-mini-4k-instruct \
+    --model_ckpt mistralai/Mistral-7B-v0.1 \
     --dataset_name mbpp_Mistral-7B-v0.1 \
-    --tensor_parallel_size 1
+    --tensor_parallel_size 2 \
